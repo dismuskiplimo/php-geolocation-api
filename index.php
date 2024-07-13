e9278a111c950b4595ff67bf35d9fd1cf902222b
Dismus Ng'eno <dismuskiplimo@gmail.com>
2024-07-14 01:06:21 +0300

    Updated README

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
     // get the client ip address
     $ip = $_SERVER['REMOTE_ADDR'];
 
-    echo 'Client IP Address: ' . $ip;
-
     $location_data = ip_api_geolocation($ip);
 
+    echo 'Client IP Address: ' . $ip . '<br>';
+
     // display location data
-    echo 'Country:' . $location_data->country . '<br>';
-    echo 'Country Code:' . $location_data->countryCode . '<br>';
-    echo 'Region:' . $location_data->region . '<br>';
-    echo 'Region Name:' . $location_data->regionName . '<br>';
-    echo 'City:' . $location_data->city . '<br>';
-    echo 'Zip:' . $location_data->zip . '<br>';
-    echo 'Latitude:' . $location_data->lat . '<br>';
-    echo 'Longitude:' . $location_data->lon . '<br>';
-    echo 'Timezone:' . $location_data->timezone . '<br>';
-    echo 'ISP:' . $location_data->isp . '<br>';
-    echo 'Organization:' . $location_data->org . '<br>';
-    echo 'As:' . $location_data->as . '<br>';
+    echo 'Country: ' . $location_data->country . '<br>';
+    echo 'Country Code: ' . $location_data->countryCode . '<br>';
+    echo 'Region: ' . $location_data->region . '<br>';
+    echo 'Region Name: ' . $location_data->regionName . '<br>';
+    echo 'City: ' . $location_data->city . '<br>';
+    echo 'Zip: ' . $location_data->zip . '<br>';
+    echo 'Latitude: ' . $location_data->lat . '<br>';
+    echo 'Longitude: ' . $location_data->lon . '<br>';
+    echo 'Timezone: ' . $location_data->timezone . '<br>';
+    echo 'ISP: ' . $location_data->isp . '<br>';
+    echo 'Organization: ' . $location_data->org . '<br>';
+    echo 'As: ' . $location_data->as . '<br>';
