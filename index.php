51b4babfb1e4ae6defa765eb417252b872f230ce
Dismus Ng'eno <dismuskiplimo@gmail.com>
2024-07-14 00:56:57 +0300

    Added Sample Resul

--- index.php
+++ index.php
@@ -8,4 +8,18 @@
 
     echo 'Client IP Address: ' . $ip;
 
-    $location_date = ip_api_geolocation($ip);
+    $location_data = ip_api_geolocation($ip);
+
+    // display location data
+    echo 'Country:' . $location_data->country . '<br>';
+    echo 'Country Code:' . $location_data->countryCode . '<br>';
+    echo 'Region:' . $location_data->region . '<br>';
+    echo 'Region Name:' . $location_data->regionName . '<br>';
+    echo 'City:' . $location_data->city . '<br>';
+    echo 'Zip:' . $location_data->zip . '<br>';
+    echo 'Latitude:' . $location_data->lat . '<br>';
+    echo 'Longitude:' . $location_data->lon . '<br>';
+    echo 'Timezone:' . $location_data->timezone . '<br>';
+    echo 'ISP:' . $location_data->isp . '<br>';
+    echo 'Organization:' . $location_data->org . '<br>';
+    echo 'As:' . $location_data->as . '<br>';
